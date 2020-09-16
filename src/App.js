587a4becd065169dc5cb1cfa2e780ef3b1b3e97e
Pawan RK <pawan@clinikk.com>
2020-09-16 20:57:20 +0530

    lotties

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 import UncontrolledLottie from './components/UncontrolledLottie';
+import UncontrolledLottie2 from './components/UncontrolledLottie2';
 import ControlledLottie from './components/ControlledLottie';
-
+import ControlledLottie2 from './components/ControlledLottie2';
+import YoutubeVideo from './components/YoutubeVideo';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hey, Pawan here, this is an example of REACT LOTTIES</h1>
         <div className="lotties">
-          <UncontrolledLottie />
-          <ControlledLottie />
+          <UncontrolledLottie2 />
+          {/* <ControlledLottie /> */}
+          <ControlledLottie2 />
         </div>
+
+        {/* <iframe src='https://youtu.be/6_pru8U2RmM'
+        frameborder='0'
+        allow='autoplay; encrypted-media'
+        allowfullscreen
+        title='video'
+/>
+
+        <video width="750" height="500" controls >
+          <source src="https://youtu.be/6_pru8U2RmM" type="video/mp4" />
+        </video> */}
+
+        <YoutubeVideo/>
       </div>
     );
   }
---
+++ src/components/UncontrolledLottie2.js
@@ -0,0 +1,33 @@
+// UncontrolledLottie.jsx
+import React, { Component } from 'react'
+import Lottie from 'react-lottie'
+import animationData from '../lotties/fight-corona.json'
+
+class UncontrolledLottie2 extends Component {
+
+
+  render(){
+
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
+
+    return(
+      <div>
+        <h1>Lottie</h1>
+        <p>Base animation free from external manipulation</p>
+        <Lottie options={defaultOptions}
+              height={400}
+              width={400}
+        />
+      </div>
+    )
+  }
+}
+
+export default UncontrolledLottie2
